b7ee72d199d5aafd8419eaa8f0b0a06276148c3c
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-21 17:55:09 +0100

    create mobile nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 
 const Navbar = () => {
@@ -10,12 +10,28 @@ const Navbar = () => {
         </div>
 
         {/* Navbar Links */}
-        <ul className='flex items-center gap-4'>
+        <ul className='flex items-center gap-4 hidden'>
             <li>Home</li>
             <li>About</li>
             <li>Services</li>
             <li>Contact</li>
         </ul> 
+
+        {/* CTA Button */}
+        <div>
+            <AiOutlineMenu size={20} />
+        </div>
+
+        {/* Mobile Menu */}
+        <div className='fixed left-0 top-0 w-4/12 h-full border-r border-r-gray-900 bg-yellow-100'>
+            <ul className='p-4 uppercase'>
+                <li className='p-4 border-b border-gray-600'>Home</li>
+                <li className='p-4 border-b border-gray-600'>About</li>
+                <li className='p-4 border-b border-gray-600'>Services</li>
+                <li className='p-4 border-b border-gray-600'>Contact</li>
+            </ul>
+        </div>
+        
     </nav>
   )
 }
