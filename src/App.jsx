7ac90d748eb3d9a046f204ebae6cbb3f4b960cb7
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-21 16:46:18 +0100

    Initialize project and set dependencies

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-400 font-medium text-4xl'>Lorem ipsum dolor sit amet.</div>
+    </>
+  )
+}
+
+export default App
