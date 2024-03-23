646f6f2c2b85305f03e80d567a968baf6c705057
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-23 08:40:59 +0100

    Added structure to Partners

--- src/Components/Partners.jsx
+++ src/Components/Partners.jsx
@@ -1,1 +1,22 @@
-rafce
+import React from 'react'
+
+const Partners = () => {
+  return (
+    <section>
+        {/* container */}
+        <div className='container'>
+            {/* heading */}
+            <div className='p-2'>
+                <h2>Lorem ipsum dolor, sit amet consectetur adipisicing.</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas expedita molestiae fugit atque accusamus minima.</p>
+            </div>
+            {/* Partners logo */}
+            <div>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Partners
