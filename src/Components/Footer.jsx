03f0ef89897d29ce0da1f8c58bfc56014d090276
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-25 09:32:01 +0100

    imported social icons

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import {FaFacebookSquare,
+        FaGithubSquare,
+        FaLinkedinIn,
+        FaInstagramSquare,
+        FaTwitterSquare,} from 'react-icons/fa'
 
 const Footer = () => {
   return (
