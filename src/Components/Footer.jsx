56eb2075b33d15a3352c3e56817c97781dd8c9ac
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2024-03-25 11:24:25 +0100

    Modified footer styles

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,38 +7,43 @@ import {FaFacebookSquare,
 
 const Footer = () => {
   return (
-    <footer className='w-full py-12'>
+    <footer className='w-full pt-10 mt-20 bg-gray-100'>
         {/* container */}
-        <div className="container grid lg:grid-cols-3 gap-8 text-gray-300 max-w-[1240px] mx-auto px-4 py-4">
+        <div className="container grid lg:grid-cols-3 gap-8 text-gray-300 mx-auto px-4 py-4 mb-0">
             {/* Column one */}
-            <div>
+            <div className='p-4'>
                 {/* Text */}
-                <div className=''>
+                <div>
                 <span className='text-3xl text-yellow-400'>Prime</span><span className='font-medium text-green-900 text-3xl'>Time</span>
                 <p className='text-base text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nihil quasi beatae rerum, quam dolorem?</p>
             </div>
             {/* socials */}
-            <div className='flex justify-between my-6 md:75%'>
-                <FaFacebookSquare  size={30} />
-                <FaInstagramSquare size={30} />
-                <FaTwitterSquare size={30} />
-                <FaLinkedinIn size={30} />
-                <FaGithubSquare size={30} />
+            <div>
+                <ul className='flex justify-between my-6 md:75% text-green-500'>
+                    <li className='hover:text-primaryColor'><FaFacebookSquare  size={30} /></li>
+                    <li className='hover:text-primaryColor'><FaInstagramSquare size={30} /></li>
+                    <li className='hover:text-primaryColor'><FaTwitterSquare size={30} /></li>
+                    <li className='hover:text-primaryColor'><FaLinkedinIn size={30} /></li>
+                    <li className='hover:text-primaryColor'><FaGithubSquare size={30} /></li>
+
+
+                </ul>
+                
             </div>
         </div>
         {/* Column-two */}
-        <div className='flex justify-between mt-4 lg:col-span-2'>
+        <div className='flex justify-between mt-4 p-2 lg:col-span-2 text-gray-600'>
             <div>
-                <h3 className='text-medium text-xl'>Solutions</h3>
-                <ul>
+                <h3 className='font-medium text-xl text-primaryColor py-2'>Solutions</h3>
+                <ul className='footer_links'>
                     <li className='py-2 text-sm'>Agile Methods</li>
                     <li className='py-2 text-sm'>SCRUM Technique</li>
                     <li className='py-2 text-sm'>Waterfall Model</li>
                 </ul>
             </div>
             <div>
-                <h3>Company</h3>
-                <ul>
+                <h3 className='font-medium text-xl text-primaryColor py-2'>Company</h3>
+                <ul className='footer_links'>
                     <li className='py-2 text-sm'>About</li>
                     <li className='py-2 text-sm'>Services</li>
                     <li className='py-2 text-sm'>Careers</li>
@@ -46,8 +51,8 @@ const Footer = () => {
             </div>
             {/* third Link column */}
             <div>
-                <h3>Support</h3>
-                <ul>
+                <h3 className='font-medium text-xl text-primaryColor py-2'>Support</h3>
+                <ul className='footer_links'>
                     <li className='py-2 text-sm'>Pricing</li>
                     <li className='py-2 text-sm'>Documentation</li>
                     <li className='py-2 text-sm'>Guides</li>
@@ -56,6 +61,10 @@ const Footer = () => {
 
         </div>
             
+        </div>
+
+        <div className='text-center pb-10 text-base text-gray-400 -mt-6'>
+            <p>PrimeTime &#169; 2024 by Sochuks&#129514;</p>
         </div>
 
     </footer>
